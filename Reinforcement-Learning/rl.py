5ff7c50e66d723f7745562802aba183034ade0b7
Kaloina <kaloina4@gmail.com>
2026-09-17 19:44:53 +0300

    Q-table

--- Reinforcement-Learning/rl.py
+++ Reinforcement-Learning/rl.py
@@ -18,6 +18,20 @@ maze = np.array([
 start = (0, 0)
 goal = (9, 9)
 
-print(start)
-print(goal)
-print(maze)
+# print(start)
+# print(goal)
+# print(maze)
+
+num_episodes = 5000
+alpha = 0.1
+gamma = 0.9
+epsilon = 0.5
+
+reward_fire = -10
+reward_goal = 50
+reward_step = -1
+
+actions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+
+Q = np.zeros(maze.shape + (len(actions),))
+print(Q)
